20929ab3058556b4946f93038b229d3fbbc62c12
Hariz S. <me@bomberfish.ca>
2026-06-15 17:24:32 -0400

    Initial Commit

---
+++ SmartNamingPlayground/SmartNamingPlaygroundApp.swift
@@ -0,0 +1,17 @@
+//
+//  SmartNamingPlaygroundApp.swift
+//  SmartNamingPlayground
+//
+//  Created on 2026-06-15.
+//
+
+import SwiftUI
+
+@main
+struct SmartNamingPlaygroundApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
